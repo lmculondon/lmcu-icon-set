f519d1125a8cfdbe5bc2206be0463bc86241b658
benjeewest <ben@benwest.me>
2021-04-09 13:32:23 +0100

    First Viable Product

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
-var iconfont = require('gulp-iconfont');
-var runTimestamp = Math.round(Date.now()/1000);
+const gulp = require('gulp');
+const svgo = require('gulp-svgo');
+const iconfont = require('gulp-iconfont');
+const iconfontCss = require('gulp-iconfont-css');
+const runTimestamp = Math.round(Date.now() / 1000);
+var LMCUFontName = 'lmcu-icons';
 
-gulp.task('Iconfont', function(){
-  return gulp.src(['src/svg/*.svg'])
+gulp.task('default', function () {
+  return gulp.src(['src/svg/**/*.svg'])
+    .pipe(svgo())
+    .pipe(iconfontCss({
+      fontName: LMCUFontName,
+      targetPath: LMCUFontName +'.css',
+    }))
     .pipe(iconfont({
-      fontName: 'myfont', // required
+      fontName: LMCUFontName, // required
+      normalize: true,
       prependUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      formats: ['ttf', 'eot', 'woff', 'svg', 'woff2'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
-      .on('glyphs', function(glyphs, options) {
-        // CSS templating, e.g.
-        console.log(glyphs, options);
-      })
+    .on('glyphs', function (glyphs, options) {
+      // CSS templating, e.g.
+      console.log(glyphs, options);
+    })
     .pipe(gulp.dest('docs/fonts/'));
 });
